5c08642b3ed8c1d1b87f025e21a99db3ad6b05cb
Armando Salazar <armandosalazar351@gmail.com>
2023-01-12 19:58:18 -0600

    Add modules

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 """
 10 de enero 0:48
 """
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import os
+# import cv2
+# import matplotlib.pyplot as plt
+# from tqdm import
+
+# os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+path = './images'
+files = os.listdir(path)
+files.sort()
+
+print(files)
+print(dir([]))
+print(type(files))
