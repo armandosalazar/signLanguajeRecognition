2bc1ae6b32a0e92d325665654bd679cf621cb3d3
Armando Salazar <armandosalazar351@gmail.com>
2023-01-13 22:32:25 -0600

    Add All dependencies for Traiding model

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import os
-# import cv2
-# import matplotlib.pyplot as plt
-# from tqdm import
+import cv2
+import matplotlib.pyplot as plt
+from tqdm import
 
 # os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
